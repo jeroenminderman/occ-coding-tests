f10260a5b8ab63e1cc89f8ec85a47b4585625a9c
Jeroen Minderman <Jeroen.Minderman@ons.gov.uk>
2025-11-19 16:24:09 +0000

    Started adding functionality to build vector base for classifai and sourcing of ISCO base data

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 import pandas as pd
+import os
+import utils
 from validator import CheckBenchmarkData
-from utils import count_matches
+#from utils import count_matches
+#from utils import get_isco_scheme_data
+#from utils import process_excel_to_csv
 from oc3i import Coder
 
+ISCO_DATA_SOURCE = "https://raw.githubusercontent.com/datasciencecampus/occupationcoder-international/main/data/ISCO-08%20EN%20Structure%20and%20definitions.xlsx"
 ISCO_DATA_FILE = "data/ISCO-08-scheme.xlsx"
+PROCESSED_ISCO_DATA = "data/ISCO-08-processed.csv"
 BENCHMARK_DATA_FILE = "data/isco_benchmark_data.csv"
 allowed_types = {"Exact match", "Semantic ambiguity", "Scheme ambiguity", "Deeper ambiguity", "Input issue", "Exact match fail"}
 columns_to_check = ["ID","TITLE","TASKS","INDUSTRY","MANUAL_ISCO1","MANUAL_ISCO2","MANUAL_ISCO3","TYPE","COMMENTS"]
 
 benchmark_dat = pd.read_csv(BENCHMARK_DATA_FILE, dtype={'MANUAL_ISCO1': str, 'MANUAL_ISCO2': str, 'MANUAL_ISCO3': str})
+if not os.path.exists(ISCO_DATA_FILE):
+    get_isco_scheme_data(source_url=ISCO_DATA_SOURCE, local_file_path=ISCO_DATA_FILE)
+
 isco_scheme_dat = pd.read_excel(ISCO_DATA_FILE, dtype={'ISCO 08 Code': str, 'Tasks include': str}, keep_default_na=False)
 
 allowed_isco_codes = isco_scheme_dat["ISCO 08 Code"]
@@ -28,6 +37,9 @@ print(report)
 
 if not report["message"].eq("OK").all():
     raise ValueError("Some validation checks on benchmark data failed, bailing out, sorry!")
+
+# Process ISCO data for use with classifai
+utils.process_excel_to_csv(ISCO_DATA_FILE, PROCESSED_ISCO_DATA)
 
 # Initialise the coder with the desired coding scheme (here ISCO):
 coder = Coder(scheme = "isco")
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 def count_matches(df: pd.DataFrame, match_col: str, prediction_cols: list, output: str = "absolute"):
     """
@@ -45,3 +46,58 @@ def count_matches(df: pd.DataFrame, match_col: str, prediction_cols: list, outpu
             "pred1_match": pred1_match.sum(),
             "any_pred_match": any_pred_match.sum()
         }
+    
+def process_excel_to_csv(input_file: str, output_file: str, code_digit_threshold: int = 4):
+    """
+    Reads an Excel file containing occupation codes and related text fields, filters rows based on the length of the occupation code,
+    concatenates relevant text fields, and saves the processed data to a CSV file.  
+    Parameters
+    ----------
+    input_file : str
+        Path to the input Excel file.
+    output_file : str
+        Path to the output CSV file.
+    code_digit_threshold : int, optional (default = 4)
+        Minimum length of the occupation code to include a row in the output.
+    Returns
+    -------
+    None
+    """
+    df = pd.read_excel(
+        input_file,
+        sheet_name='ISCO-08 EN Struct and defin',
+        engine='openpyxl',
+        dtype={'ISCO 08 Code': str}
+    )
+
+    df = df[df['ISCO 08 Code'].str.len() >= code_digit_threshold]
+
+    df.fillna('', inplace=True)
+
+    df['text'] = df['Title EN'] + ' ' + df['Definition'] + ' ' + df['Tasks include'] + ' ' + df['Included occupations']
+
+    result_df = df[['ISCO 08 Code', 'Title EN', 'text']].rename(columns={
+        'ISCO 08 Code': 'id',
+        'Title EN': 'title'
+    })
+
+    result_df.to_csv(output_file, index=False)
+    print(f"Filtered and processed data saved to {output_file}")
+
+
+def get_isco_scheme_data(source_url: str, local_file_path: str):
+    """
+    Downloads the ISCO scheme data from the specified URL and saves it to a local file.
+    Parameters
+    ----------
+    source_url : str
+        URL to download the ISCO scheme data from.  
+    local_file_path : str
+        Path to save the downloaded ISCO scheme data.
+    Returns
+    -------
+    None
+    """
+    response = requests.get(source_url)
+    with open(local_file_path, "wb") as f:
+        f.write(response.content)
